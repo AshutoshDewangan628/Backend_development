c819d1a59e4a451bbfd1f89c861a6abebb24f9e4
My Name <ashutoshdewangan25@gmail.com>
2024-03-23 02:29:12 +0530

    second git

--- Day_12/myModule.js
+++ Day_12/myModule.js
@@ -1,3 +1,6 @@
 exports.myDate=function(){
     return Date();
 }
+// git remote add origin https://github.com/AshutoshDewangan628/Backend_development.git
+// git branch -M main
+// git push -u origin main
